b400de3f0e9440aa5e78f370e462d19b771d3d3c
guxukai <1378026744@qq.com>
2020-02-19 13:22:55 +0800

    kobe.mp4

--- nuxt-ts-blog/components/Video.vue
+++ nuxt-ts-blog/components/Video.vue
@@ -2,7 +2,7 @@
   <div>
     <video
       id="video"
-      src="//f.video.weibocdn.com/003jXCUelx07wmTAcnsI0104120a0wMI0E040.mp4?label=mp4_ld&template=640x360.24.0&trans_finger=78679548d3dda0964ec12b81fbdd99c2&Expires=1582082378&ssig=A8t%2BDSu8sl&KID=unistore,video"
+      src="/kobe.mp4"
       controls="true"
       poster="/kobe.JPG"
       preload="auto"
